0d67eccb01ec3bcc6d3a5d18df9b194bbf9d8eaa
Sourav0809 <spathak7431@gmail.com>
2023-12-12 19:37:33 +0530

    FIXED

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,10 +15,10 @@ const Header = () => {
       className=" text-white sticky top-0 font-custom flex justify-between items-center py-5 px-[4rem] md:px-[10rem] text-2xl bg-customBlack z-50  "
       id="main-header"
     >
-      <div className=" flex justify-center items-center gap-2 text-red-500  font-semibold">
+      <div className=" flex justify-center items-center gap-2 text-red-500  font-semibold ">
         <MdOutlineCodeOff className="cursor-pointer" />
         <h1
-          className="cursor-pointer"
+          className="cursor-pointer text-2.5xl"
           onClick={() => {
             location.reload();
           }}
@@ -27,9 +27,9 @@ const Header = () => {
         </h1>
       </div>
       <div className=" md:hidden ">
-        <GiHamburgerMenu className=" text-4xl" />
+        <GiHamburgerMenu className=" text-5xl" />
       </div>
-      <div className=" hidden md:flex justify-center items-center gap-8 text-xl  ">
+      <div className=" hidden md:flex justify-center items-center gap-8 text-2xl  ">
         <h1 className=" cursor-pointer hover:text-green-500">HOME</h1>
         <h1 className=" cursor-pointer hover:text-green-500">ABOUT</h1>
         <h1 className=" cursor-pointer hover:text-green-500">SKILLS</h1>
